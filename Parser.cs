676a6a0de0717ee0995f9f1ef4bafa2bdba2bb0a
jan0mann <jan.ferchof@icloud.com>
2024-10-11 14:47:06 +0200

    Create Parser.cs

---
+++ Parser.cs
@@ -0,0 +1,25 @@
+namespace WorldOfZuul
+{
+    public class Parser
+    {
+        private readonly CommandWords commandWords = new();
+
+        public Command? GetCommand(string inputLine)
+        {
+            string[] words = inputLine.Split();
+
+            if (words.Length == 0 || !commandWords.IsValidCommand(words[0]))
+            {
+                return null;
+            }
+
+            if (words.Length > 1)
+            {
+                return new Command(words[0], words[1]);
+            }
+
+            return new Command(words[0]);
+        }
+    }
+
+}
