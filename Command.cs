31917d13548335a5abfeee48b0c3894b2421fc64
jan0mann <jan.ferchof@icloud.com>
2024-10-11 14:45:09 +0200

    Create Command.cs

---
+++ Command.cs
@@ -0,0 +1,14 @@
+namespace WorldOfZuul
+{
+    public class Command
+    {
+        public string Name { get; }
+        public string? SecondWord { get; } // this might be used for future expansions, such as "take apple".
+
+        public Command(string name, string? secondWord = null)
+        {
+            Name = name;
+            SecondWord = secondWord;
+        }
+    }
+}
