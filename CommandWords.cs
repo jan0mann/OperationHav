0674badbb8c303083952d1ac16f8bf3c7ba90e23
jan0mann <jan.ferchof@icloud.com>
2024-10-11 14:46:15 +0200

    Create CommandWords.cs

---
+++ CommandWords.cs
@@ -0,0 +1,13 @@
+namespace WorldOfZuul
+{
+    public class CommandWords
+    {
+        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "look", "back", "quit" };
+
+        public bool IsValidCommand(string command)
+        {
+            return ValidCommands.Contains(command);
+        }
+    }
+
+}
