4511c2d8ccd53bb7728c07db7ebcc7b5491e3829
jan0mann <jan.ferchof@icloud.com>
2024-10-23 15:29:54 +0200

    ...

--- CommandWords.cs
+++ CommandWords.cs
@@ -2,7 +2,7 @@ namespace OperationHav
 {
     public class CommandWords
     {
-        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "look", "help", "back", "quit", "Hello people" };
+        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "look", "help", "back", "quit" };
 
         public bool IsValidCommand(string command)
         {
