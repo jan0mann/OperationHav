f2151a3e2107daf405e2574abb48646669dddb92
Bartosh2005 <bartorski2005@o2.pl>
2024-10-24 09:48:38 +0200

    git ignore

--- obj/Debug/net9.0/OperationHav.AssemblyInfo.cs
+++ obj/Debug/net9.0/OperationHav.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperationHav")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff81e17f0f9b8b0e402a29965cb8607a379ef46b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd117133f707f5ff6f24052658ab4b2532ec269")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperationHav")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperationHav")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
